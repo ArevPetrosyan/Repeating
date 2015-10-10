5a5e7c5d38537e31aaa2e6205ea6ef0c617decc1
ArevPetrosyan <arev.petrosyan94@gmail.com>
2015-10-11 01:28:12 +0400

    Static

---
+++ BasicsOfCSharp/Static/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Bank
+    {
+        public static int balance=10;
+        public int sum;
+        public void AddSum()
+        {
+            sum += balance;
+        }
+        public static int AddBalance(int b)
+        {
+            //return sum += b;//sum is not static
+            return balance += b;
+        }
+    }
+}
---
+++ BasicsOfCSharp/Static/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    //class data
+    //class methods
+    //class properties
+    //constructors
+    //whole class
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank myBank = new Bank();
+            Console.WriteLine("balance {0}",Bank.balance);
+            myBank.sum = 150;
+            myBank.AddSum();
+            Console.WriteLine("my sum {0}", myBank.sum);
+            Console.WriteLine("Add Balance  {0}", Bank.AddBalance(5));
+            Console.WriteLine();
+            Bank yorBank = new Bank();
+            Console.WriteLine(Bank.balance);
+            
+            Console.ReadKey();
+        }
+    }
+}
