243bbc82f4418149caae61911416e3a85c0931f4
ArevPetrosyan <arev.petrosyan94@gmail.com>
2015-10-10 23:30:00 +0400

    Enum

---
+++ BasicsOfCSharp/Enum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colors b = Colors.Black;
+            Colors r = Colors.Red;
+            Colors count = Colors.Number;
+            Console.WriteLine(b);//Black
+            Console.WriteLine(r);//Red
+            Console.WriteLine(count);//Number
+            Console.WriteLine();
+            Console.WriteLine("{0} color = {1}", count, (int)count);//Count color = 9
+
+            Console.ReadKey();
+        }
+    }
+    enum Colors
+    {
+        Red,
+        Blue,
+        Yellow,
+        Green,
+        Ornage,
+        Violet,
+        Indigo,
+        Black,
+        White,
+        Number = 9
+    }
+}
