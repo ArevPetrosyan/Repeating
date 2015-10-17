623fa09e467335dbf456c732e90aa253727747cc
Arevik Petrosyan <arev.petrosyan94@gmail.com>
2015-10-17 18:50:40 +0400

    Delegate

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+
+    delegate void Del();
+    delegate int DelInt(int x);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Del del = new Del(M1);
+            del += MM;
+
+            // del += M2;
+
+
+            DelInt delint = new DelInt(M2);
+            Console.WriteLine(delint(40));
+            del();
+            Console.WriteLine("del-M1");
+            del -= M1;
+            del();
+
+            Console.Read();
+        }
+        static void M1()
+        {
+            Console.WriteLine("M1 method");
+            Console.Read();
+        }
+        static void MM()
+        {
+            Console.WriteLine("Method MM");
+            Console.Read();
+        }
+        static int M2(int x)
+        {
+            return x * x;
+        }
+    }
+
+
+}
