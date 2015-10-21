cb1c800a07a515ffacc05909cf8e840b27b74004
Arevik Petrosyan <arev.petrosyan94@gmail.com>
2015-10-21 11:06:29 +0400

    Delegate (method, delegate)

--- Delegate 02/Delegate 02/Program.cs
+++ Delegate 02/Delegate 02/Program.cs
@@ -2,33 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Delegate_02
 {
-    delegate void myDell(int x);
+    public delegate void Dell1(int x, Dell2 d);
+    public delegate void Dell2();
+
     class Program
     {
         static void Main(string[] args)
         {
-            myDell mdell = new myDell(Start);
-            mdell += Stop;
-            mdell(100);
+            Dell2 del2 = new Dell2(Go);
+            Dell1 del1 = new Dell1(Start);
+           
+            del1(10, del2);
 
+            
+       
             Console.WriteLine();
             Console.Read();
         }
 
-        static void Start(int x)
+        public static void Start(int x, Dell2 d)
         {
-            Console.WriteLine("go is: {0}", x);
+            for (int i = 0; i < x; i++)
+            {
+                d();
+                Thread.Sleep(1000);
+                Console.Write(i);
+                Console.WriteLine();
+            }
         }
 
-        static void Stop(int y)
+        public static void Go()
         {
-            y = 0;
-            Console.WriteLine("Stop: {0}", y);
+            Console.Write("go to: ");
         }
+        
     }
 
 }
