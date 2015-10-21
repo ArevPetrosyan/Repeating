b511b71680f7a5129f7c5004f64d015d15028e01
Arevik Petrosyan <arev.petrosyan94@gmail.com>
2015-10-22 02:54:00 +0400

    Generic Delegate

---
+++ GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDelegate
+{
+    public delegate void MyGenericDelegate<T>(T arg);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericDelegate<string> stDel = new MyGenericDelegate<string>(StringTarget);
+            MyGenericDelegate<int> intDel = new MyGenericDelegate<int>(IntTarget);
+            stDel("type string");
+            intDel(15);
+
+            Console.Read();
+
+        }
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine(arg.ToUpper());
+        }
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine(++arg);
+        }
+    }
+}
