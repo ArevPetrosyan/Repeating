4d48b529c06b22c858ad722fc737786fa9cbfd3a
Arevik Petrosyan <arev.petrosyan94@gmail.com>
2015-10-28 02:44:23 +0400

    Static Constructors

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Child();
+            Console.Read();
+        }
+    }
+
+    class Parent
+    {
+        static Parent()
+        {
+            Console.WriteLine("static Parent");
+        }
+        public Parent()
+        {
+            Console.WriteLine("Pablic Parent");
+        }
+    }
+    class Child : Parent
+    {
+        static Child()
+        {
+            Console.WriteLine("static Child");
+        }
+        public Child()
+        {
+            Console.WriteLine("Pablic Child");
+        }
+    }
+}
